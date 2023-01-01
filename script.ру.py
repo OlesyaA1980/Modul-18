dfdc77fba30de1480d8c47131b0d4384b6c21b1f
OlesyaA1980 <lisenok_k80@mail.ru>
2023-01-01 19:42:55 +0500

    Initial Commit

---
+++ script.ру.py
@@ -0,0 +1,14 @@
+string = input("1 1 2 3 5 8 13 21 34 55")
+
+list_of_strings = string.split() # список строковых представлений чисел
+list_of_numbers = list(map(int, list_of_strings)) # список чисел
+
+print(sum(list_of_numbers[::3])) # sum() вычисляет сумму элементов списка
+
+
+
+
+
+
+
+
