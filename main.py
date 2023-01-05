195c6f5be76d0d0fa776f4d0e97fbbf676121b9a
OlesyaA1980 <120561802+OlesyaA1980@users.noreply.github.com>
2023-01-05 23:18:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print ("hghghg")
+all_tickets = int(input('Введите количество билетов: '))
+
+# Создаем list, в котором указывается возраст участников
+numbers_ages = []
+
+# для каждого билета запрашиваем возраст
+# Заполняем numbers_ages числами, которые вводит пользователь
+for i in range(0, all_tickets):
+    input_value = int(input(f'Введите возраст участника №{i + 1}:\n'))
+    numbers_ages.append(input_value)
+
+# исходя из возраста выбираем стоимость билета
+    def prise(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        else:
+            return 1390
+
+# считаем стоимость всех билетов
+    full_prise = sum(map(prise, numbers_ages))
+
+# считаем скидку если количество билетов больше 3
+discount_prise = int(full_prise * 0.9)
+if all_tickets > 3:
+    print('Стоимость всех билетов со скидкой: ', discount_prise, "руб.")
+else:
+    print('Стоимость всех билетов: ', full_prise, "руб.")
